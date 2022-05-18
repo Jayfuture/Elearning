b87c0e04d4d1f1da26aca7777c702988ec5d2030
linyuanmax <linyuanmax@163.com>
2022-05-18 08:57:36 -0400

    Customer

--- src/main/java/in/bushansirgur/springbootthymeleaf/dao/TeacherRepository.java
+++ src/main/java/in/bushansirgur/springbootthymeleaf/dao/TeacherRepository.java
@@ -1,10 +1,16 @@
 package in.bushansirgur.springbootthymeleaf.dao;
 
+import in.bushansirgur.springbootthymeleaf.entity.Customer;
 import in.bushansirgur.springbootthymeleaf.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Integer>{
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public Teacher findByEmail(String email);
+
+    public Teacher findByUsername(String username);
 
 }
